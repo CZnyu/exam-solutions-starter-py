602e0defa33b39c74c875bb2188de7865b50bd29
CZnyu <CZnyu@users.noreply.github.com>
2020-06-11 12:43:49 -0400

    Social Part B

--- solutions/social.py
+++ solutions/social.py
@@ -59,14 +59,17 @@ if __name__ == "__main__":
     # Of all the tweets which include the phrase "@sandwhoa" in their full text,
     # ... "print" the screen name of the user who authored that tweet,
     # ... each on a separate line (i.e. "person2", then "person3"):
-    
-    
+
+    def contains_shop(message):
+        return "@sandwhoa" in message["full_text"]
+
+    at_us = list(filter(contains_shop, tweets))
+
+    users = ([n["user"]["screen_name"] for n in at_us])
+    print(*users,sep='\n')
+
 
     
-    #user_info = ([n["user"] for n in tweets if n["full_text"] [0])
-    #print(user_info)
-
-
 
     #
     # QUESTION C
@@ -78,7 +81,7 @@ if __name__ == "__main__":
     # ... and has no relationship with the number of likes.
 
     def top_tweet(all_tweets):
-        return max(all_tweets, key=lambda best: best["likes_count"])
+         return max(all_tweets, key=lambda best: best["likes_count"])
     
     
     influencer = (top_tweet(tweets))
